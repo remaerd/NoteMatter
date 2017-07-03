12224c26dadafe208df69c4a486a16d392d16d4f
Xingzhi.Zheng <hi@zhengxingzhi.com>
2017-07-04 02:13:49 +0800

    Batch Small Commit

---
+++ GiGi.Core/MarkdownParser.swift
@@ -0,0 +1,9 @@
+//
+//  MarkdownParser.swift
+//  GiGi
+//
+//  Created by Sean Cheng on 03/07/2017.
+//  Copyright © 2017 Zheng Xingzhi. All rights reserved.
+//
+
+import Foundation
---
+++ GiGi.iOS/KeyboardToolbar.swift
@@ -0,0 +1,9 @@
+//
+//  KeyboardToolbar.swift
+//  GiGi
+//
+//  Created by Sean Cheng on 02/07/2017.
+//  Copyright © 2017 Zheng Xingzhi. All rights reserved.
+//
+
+import Foundation
