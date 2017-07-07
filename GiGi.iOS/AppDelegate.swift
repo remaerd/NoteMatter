b39abfbb431851baa75c5e0d0dadc786438511f4
Xingzhi.Zheng <hi@zhengxingzhi.com>
2017-07-07 16:55:10 +0800

    增加 Sentry

--- GiGi.iOS/AppDelegate.swift
+++ GiGi.iOS/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-import GiGi
+import Sentry
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
 	{
+		do
+		{
+			Client.shared = try Client(dsn: "https://e8cf8c7637c04d7b9864d5e50d3f4d3c:ca2dc5c69b3a4b1c88be07a937c3c080@sentry.io/188692")
+			try Client.shared?.startCrashHandler()
+		} catch
+		{
+			print(error)
+		}
+
 		window = UIWindow(frame: UIScreen.main.bounds)
 		window?.rootViewController = UINavigationController(rootViewController: LaunchViewController())
 		window?.makeKeyAndVisible()
